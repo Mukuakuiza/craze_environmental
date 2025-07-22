acb1b97a33aade3c1b1c883f3d0bc10d45a41463
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-07-22 21:48:16 +0200

    implemented all the necessary information on the solution compoenet

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,19 @@
-import Ecomoja from '../assets/images/ecomoja.webp';
-import { motion } from 'framer-motion';
+import Ecomoja from "../assets/images/ecomoja.webp";
+import { motion } from "framer-motion";
 const Cards = ({
   containerVariants,
   squareVariants,
   title,
-  bg = 'bg-green-100',
+  bg = "bg-lime-500",
 }) => {
   return (
-    <>
-      <h2 className="text-center text-3xl my-10 underline decoration-black">
-        {title}
-      </h2>
+    <div className="bg-black overflow-hidden py-24 sm:py-32">
+      <div className="text-center text-3xl my-10 text-white">
+        <h2>{title}</h2>
+        <h2 className="text-base/7 text-center font-semibold text-lime-500">
+          ------------------------
+        </h2>
+      </div>
       <div className="flex flex-col gap-10 overflow-x-hidden ">
         <motion.section
           variants={containerVariants}
@@ -18,44 +21,6 @@ const Cards = ({
           animate="show"
           className="grid grid-cols-2 p-10 gap-20 mx-32"
         >
-          <motion.div
-            variants={squareVariants}
-            className=" aspect-square rounded-lg justify-center flex items-center gap-10 text-white"
-          >
-            <motion.div
-              initial={{ opacity: 0, y: -100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, ease: 'easeOut', delay: 1 }}
-            >
-              <p className="mt-2  text-black  text-2xl">
-                At Craze Environmental (Pty) Ltd. we understand that our
-                well-being, improved quality of life and a secure world for the
-                present and future generations depend on the transition to a
-                more sustainable lifestyle. Thus, we are proactive and optimize
-                innovative thinking to achieve sustainability.{' '}
-              </p>
-              <p className="mt-2 mb-20 text-black  text-2xl">
-                We are an Environmental technology startup. We are currently
-                building our platform EcoMoja to help people transition to an
-                eco-friendly lifestyle. A lifestyle that is good for people and
-                the planet. Convenient Eco-friendly lifestyle at your
-                fingertips.
-              </p>
-              <motion.a
-                whileTap={{ scale: 0.9 }}
-                whileHover={{
-                  scale: 1.1,
-                  backgroundColor: 'black',
-                  color: 'white',
-                }}
-                transition={{ bounceDamping: 10, bounceStiffness: 600 }}
-                className="inline-block bg-black p-10 text-white rounded-lg px-4 py-2 hover:bg-zinc-900 cursor-pointer"
-              >
-                More on EcoMoja
-              </motion.a>
-            </motion.div>
-          </motion.div>
-
           <motion.div
             variants={squareVariants}
             // className="bg-green-100 rounded-lg justify-center flex size-92 items-center gap-10"
@@ -66,11 +31,11 @@ const Cards = ({
               animate={{
                 scale: [1, 2, 2, 1],
                 rotate: [90, 0, 0, 90],
-                borderRadius: ['10%', '10%', '50%', '10%'],
+                borderRadius: ["10%", "10%", "50%", "10%"],
               }}
               transition={{
                 duration: 5,
-                ease: 'easeInOut',
+                ease: "easeInOut",
                 repeat: Infinity,
                 repeatDelay: 1,
               }}
@@ -78,9 +43,38 @@ const Cards = ({
               <img className="rounded-lg" src={Ecomoja}></img>
             </motion.div>
           </motion.div>
+
+          <motion.div
+            variants={squareVariants}
+            className=" aspect-square rounded-lg justify-center flex items-center gap-10 text-white"
+          >
+            <motion.div
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+            >
+              <p className="mt-2  text-white  text-2xl">
+                At Craze Environmental (Pty) Ltd. we understand that our
+                well-being, improved quality of life and a secure world for the
+                present and future generations depend on the transition to a
+                more sustainable lifestyle. Thus, we are proactive and optimize
+                innovative thinking to achieve sustainability.{" "}
+              </p>
+              <p className="mt-2  text-white  text-2xl">
+                We are an Environmental technology startup. We are currently
+                building our platform EcoMoja to help people transition to an
+                eco-friendly lifestyle. A lifestyle that is good for people and
+                the planet. Convenient Eco-friendly lifestyle at your
+                fingertips.
+              </p>
+              <h2 className="text-base/7 mt-4 text-end  font-semibold text-lime-500">
+                -----------------------------------------
+              </h2>
+            </motion.div>
+          </motion.div>
         </motion.section>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/SpecialitiesCard.jsx
+++ src/components/SpecialitiesCard.jsx
@@ -1,33 +1,35 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 const SpecialitiesCard = ({ containerVariants, squareVariants, title }) => {
   return (
     <>
-      <h2 className="text-center text-3xl underline my-12 decoration-black">
-        {title}
-      </h2>
-      <div className="flex bg-green-100 flex-col gap-10">
+      <div className="flex bg-lime-500 flex-col gap-10">
+        <div>
+          <h2 className="text-center text-3xl mt-36">{title}</h2>
+          <h2 className="text-base/7 text-center font-semibold text-black">
+            ------------------------
+          </h2>
+        </div>
+
         <motion.section
           variants={containerVariants}
           initial="hidden"
           animate="show"
-          className="grid grid-cols-3 p-10 gap-10  my-32 mx-32"
+          className="grid grid-cols-3 gap-10  my-32 mx-32"
         >
-          {/*Fade up */}
           <motion.div
             variants={squareVariants}
-            className="bg-[url('/images/envi-4.jpg')] bg-hero max-h-full aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
+            className="bg-[url('/images/envi-4.jpg')] aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
           >
-            <h2 className="text-center text-2xl text-white mt-10">
+            <h2 className="text-center text-2xl text-white mt-10 bg-lime-500">
               Environmental Management
             </h2>
           </motion.div>
 
-          {/*Shape Shifting */}
           <motion.div
             variants={squareVariants}
-            className="bg-[url('/images/sustainable-product.jpg')] bg-hero max-h-full aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
+            className="bg-[url('/images/sustainable-product.jpg')] bg-lime-500 max-h-full aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
           >
-            <h2 className="text-center text-2xl text-white mt-10">
+            <h2 className="text-center text-2xl text-white mt-10 bg-lime-500">
               Sustainable Product Development
             </h2>
           </motion.div>
@@ -36,27 +38,25 @@ const SpecialitiesCard = ({ containerVariants, squareVariants, title }) => {
             variants={squareVariants}
             className="bg-[url('/images/envi-3.jpg')] bg-hero max-h-full aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
           >
-            <h2 className="text-center text-2xl text-white mt-10">
+            <h2 className="text-center text-2xl text-white mt-10 bg-lime-500">
               Waste Management
             </h2>
           </motion.div>
 
-          {/*Drag */}
           <motion.div
             variants={squareVariants}
             className="bg-[url('/images/Sustainable-Consumption.jpg')] bg-hero max-h-full aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
           >
-            <h2 className="text-center text-2xl text-white mt-10">
+            <h2 className="text-center text-2xl text-white mt-10 bg-lime-500">
               Sustainable Consumption and Production
             </h2>
           </motion.div>
 
-          {/*Scroll Progress */}
           <motion.div
             variants={squareVariants}
             className="bg-[url('/images/Sustainable-Transportation.jpg')] bg-hero max-h-full aspect-square rounded-lg bg-no-repeat py-52 mb-4 bg-cover bg-top bg-fixed"
           >
-            <h2 className="text-center text-2xl text-white mt-10">
+            <h2 className="text-center text-2xl text-white mt-10 bg-lime-500">
               Sustainable Transportation
             </h2>
           </motion.div>
@@ -66,7 +66,7 @@ const SpecialitiesCard = ({ containerVariants, squareVariants, title }) => {
             variants={squareVariants}
             className="bg-[url('/images/Environmental-Software.jpg')] bg-hero max-h-full aspect-square rounded-lg bg-no-repeat  py-52 mb-4 bg-cover bg-center bg-fixed"
           >
-            <h2 className="text-center text-2xl text-white mt-10">
+            <h2 className="text-center text-2xl text-white mt-10 bg-lime-500">
               Environmental Software Development
             </h2>
           </motion.div>
